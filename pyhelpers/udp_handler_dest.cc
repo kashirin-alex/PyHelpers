c86e47abe0e7532a424fd1f506b8e8c8d0545262
Kashirin Alex <kashirin.alex@gmail.com>
2019-02-26 22:09:06 +0200

    new udp_handler_dest module class UdpHandlerDest

---
+++ pyhelpers/udp_handler_dest.cc
@@ -0,0 +1,621 @@
+
+/* AUTHOR Kashirin Alex (kashirin.alex@gmail.com) */
+
+#include <iostream>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/eventfd.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+#include <chrono>
+#include <array>
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+namespace PyHelpers {
+
+
+// QueueItemMsg
+struct QueueItemMsg {
+  std::string   addr;
+  int           port;
+  std::string   data;
+  int64_t       ts_ns;
+};
+
+// QueueItemFd
+typedef int QueueItemFd;
+
+
+// Blockable Thread-Safe Queue<ItemType>
+template <class T>
+class Queue : std::enable_shared_from_this<Queue<T>>{
+
+  public:
+
+    Queue(){}
+
+    size_t size(){
+      std::lock_guard<std::mutex> lock(m_mutex);
+      return m_queue.size();
+    }
+
+    T pop_first(){
+      std::lock_guard<std::mutex> lock(m_mutex);
+      
+      T item;
+      if(m_queue.size() == 0)
+        return item;
+      
+      item = *(m_queue.front().get());
+      m_queue.erase(m_queue.begin());
+      return item;
+    }
+
+
+    void push_back(T item){
+      {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_queue.push_back(std::make_shared<T>(item));
+      }
+      m_blocker_cv.notify_one();
+    }
+
+    void should_block(){
+      std::unique_lock<std::mutex> lock(m_blocker);
+
+      while(!m_stopping&&size() == 0)
+        m_blocker_cv.wait(lock);
+    }
+
+    void stopping(){
+      m_stopping.store(true);
+      m_blocker_cv.notify_all();
+    }
+
+    virtual ~Queue(){}
+
+  private:
+    std::mutex m_mutex;
+    std::vector<std::shared_ptr<T>> m_queue;
+    
+    std::condition_variable m_blocker_cv;
+    std::mutex m_blocker;
+
+    std::atomic<bool> m_stopping = false;
+
+};
+typedef std::shared_ptr<Queue<QueueItemMsg>>  QueueMsgPtr;
+typedef std::shared_ptr<Queue<QueueItemFd>>   QueueFdPtr;
+
+
+struct DestAddr {
+  struct sockaddr* addr;
+  socklen_t        len;
+  
+  struct sockaddr_in  da4 = {0};
+  struct sockaddr_in6 da6 = {0};
+};
+
+int64_t ts_ns_now(){
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+     std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+bool make_socket_nonblocking(int socketfd) {
+  int flags = fcntl(socketfd, F_GETFL, 0);
+  if (flags == -1) {
+    std::cerr << "[E] fcntl failed (F_GETFL)\n";
+    return false;
+  }
+  if(flags & O_NONBLOCK)
+    return true;
+
+  flags |= O_NONBLOCK;
+  int s = fcntl(socketfd, F_SETFL, flags);
+  if (s == -1) {
+    std::cerr << "[E] fcntl failed (F_SETFL)\n";
+    return false;
+  }
+  return true;
+}
+
+// UdpDispatcher
+class UdpDispatcher : std::enable_shared_from_this<UdpDispatcher>{
+
+  public:
+
+    UdpDispatcher(int sock_fileno, int snd_flags,  QueueMsgPtr queue, bool debug):
+                  m_fd(dup(sock_fileno)), m_snd_flags(snd_flags), m_queue(queue), 
+                  m_debug(debug) {}
+
+    void run(){
+
+        while(1){
+          m_queue->should_block();
+
+          if(!m_run.load()) 
+            break;
+
+          dispatch_item(m_queue->pop_first());
+          
+          if(errno == EBADF || errno == ENOTCONN)
+            break;
+        }
+
+        try { close(m_fd); } catch (...) {}
+    }
+
+    void shutdown(){
+      m_run.store(false);
+    }
+
+    virtual ~UdpDispatcher(){
+      shutdown();
+    }
+
+    std::atomic<uint32_t>  m_stat_sendto_c=0;
+    std::atomic<uint32_t>  m_stat_sendto_avg=0;
+
+  private:
+
+    void dispatch_item(QueueItemMsg msg){
+      
+      if(msg.addr.empty()) return;
+
+      if(m_debug >= 5)
+        std::cout << "UdpDispatcher dispatch_item, " 
+                  << " addr " << msg.addr 
+                  << " port " << msg.port 
+                  << " data " << msg.data 
+                  << " ts_ns " << msg.ts_ns 
+                  << "\n";
+
+      errno = 0;
+          
+      DestAddr to = {0};
+      set_dest_addr(msg.addr, htons(msg.port), &to);
+
+      if(errno == 0 && sendto(m_fd, msg.data.c_str(), msg.data.length(), 
+                              m_snd_flags, to.addr, to.len)  > -1){
+
+        m_stat_sendto_avg.store((m_stat_sendto_avg * m_stat_sendto_c + (ts_ns_now() - msg.ts_ns)) 
+                                / (m_stat_sendto_c + 1));
+        m_stat_sendto_c++;
+        return;
+      }
+
+      std::cerr << "Error dispatch_item: " << msg.addr << ":" << msg.port 
+                << " data-sz: " << msg.data.length()
+                << " error: " << errno << " " << strerror(errno) << "\n";
+    }
+
+    static void set_dest_addr(std::string addr, in_port_t port, DestAddr *to) {
+      
+      if(inet_pton(AF_INET, addr.c_str(), &to->da4.sin_addr.s_addr) == 1){
+        to->da4.sin_family=AF_INET;
+        to->da4.sin_port=port;
+        to->len = sizeof(to->da4);
+        to->addr = (struct sockaddr*)&to->da4;
+        return;
+      }
+      if (inet_pton(AF_INET6, addr.c_str(), &to->da6.sin6_addr) == 1){
+        to->da6.sin6_family=AF_INET6;
+        to->da6.sin6_port=port;
+        to->len = sizeof(to->da6);
+        to->addr = (struct sockaddr*)&to->da6;
+        return;
+      }
+
+    }
+
+    std::atomic<bool>      m_run=true;
+
+    int m_fd, m_snd_flags;
+    QueueMsgPtr m_queue;
+
+    int m_debug;
+
+};
+typedef std::shared_ptr<UdpDispatcher> UdpDispatcherPtr;
+// UdpDispatcher end
+
+
+
+// UdpReceiver
+class UdpReceiver : std::enable_shared_from_this<UdpReceiver>{
+
+  public:
+
+    UdpReceiver(int ev_fileno, int sendbuff_sz, QueueMsgPtr q_received, QueueFdPtr q_fds, bool debug):
+                m_ev_fd(ev_fileno), m_sendbuff_sz(sendbuff_sz),
+                m_queue_received(q_received), m_queue_fds(q_fds), 
+                m_debug(debug) {}
+
+    void run(){
+      int fd;
+      bool rcv;
+      uint64_t  count = 0;
+      int ev_sent;
+
+      while(1){
+        m_queue_fds->should_block();
+
+        if(!m_run.load()) 
+          break;
+        
+        fd = m_queue_fds->pop_first();
+        
+        if(m_debug >= 4)
+          std::cout << "UdpReceiver run, " << "fd " << fd  << "\n";
+        if(fd < 1)
+          continue;
+
+        rcv = receive(fd);
+        
+        if(m_debug >= 5 && errno != 0)
+          std::cout << "UdpReceiver run, " << "fd " << fd 
+                    <<"errno " << errno << " " << strerror(errno) << "\n";
+
+        if(errno == EBADF || errno == ENOTCONN)
+          break;
+        if(!rcv)
+          continue;
+          
+        count++;
+        if(count >= INT64_MAX )
+          count = 1;
+        errno = 0; 
+        if((ev_sent = write(m_ev_fd, &count, sizeof(uint64_t))) > 0)
+          count--;
+        if(errno != 0)
+          std::cerr << "Error ev_write: " << errno << " " << strerror(errno) << "\n";
+
+        if(ev_sent == -1 && (errno == EBADF || errno == ENOTCONN))
+          break;
+      }
+
+      try { close(m_ev_fd); } catch (...) {}
+    }
+
+    void shutdown(){
+      m_run.store(false);
+    }
+
+    virtual ~UdpReceiver(){
+      shutdown();
+    }
+
+  private:
+
+    bool receive(int sockfd){
+
+      errno = 0; 
+
+      char buf[m_sendbuff_sz];
+      memset(buf, 0, sizeof(buf));
+
+      struct sockaddr_storage caddr;
+      socklen_t caddrlen = sizeof(caddr);
+
+      ssize_t nbytes;
+      
+      if((nbytes = recvfrom(sockfd, buf, m_sendbuff_sz, 0,(struct sockaddr *) &caddr, &caddrlen)) < 1){
+        return false;
+      }
+
+      QueueItemMsg msg = {};
+
+      char c_addr[INET6_ADDRSTRLEN];
+      switch(caddr.ss_family){
+        case AF_INET: {
+          inet_ntop(AF_INET, &((struct sockaddr_in *)&caddr)->sin_addr,  c_addr, INET6_ADDRSTRLEN);
+          if(c_addr == nullptr) 
+            return false;
+          msg.port = ntohs(((struct sockaddr_in *)&caddr)->sin_port);
+          break;
+        }
+        case AF_INET6:{
+          inet_ntop(AF_INET6, &((struct sockaddr_in6 *)&caddr)->sin6_addr, c_addr, INET6_ADDRSTRLEN);
+          if(c_addr == nullptr) 
+            return false;
+          msg.port = ntohs(((struct sockaddr_in6 *)&caddr)->sin6_port);
+          break;
+        }
+       default:
+         return false;
+      }
+
+      msg.addr = c_addr;
+      msg.data = buf;
+      msg.ts_ns = ts_ns_now();
+      m_queue_received->push_back(msg);
+
+      if(m_debug >= 5)
+        std::cout << "UdpReceiver receive, " 
+                  << " addr " << msg.addr 
+                  << " port " << msg.port 
+                  << " data " << msg.data 
+                  << " ts_ns " << msg.ts_ns 
+                  << "\n";
+      return true;
+    }
+
+    std::atomic<bool>      m_run=true;
+
+    int m_ev_fd, m_sendbuff_sz;
+    QueueMsgPtr m_queue_received;
+    QueueFdPtr m_queue_fds;
+
+    int m_debug;
+    
+};
+typedef std::shared_ptr<UdpReceiver> UdpReceiverPtr;
+// UdpReceiver end
+
+
+
+// Epoll
+class Epoll : std::enable_shared_from_this<Epoll>{
+
+  public:
+  
+    Epoll(int poll_fd, int max_evs, QueueFdPtr q_fds, bool debug): 
+          m_poll_fd(poll_fd), m_max_evs(max_evs), m_queue_fds(q_fds), m_debug(debug) {}
+
+    void run(){
+      int nfds;
+      
+      struct epoll_event *events;
+      events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * m_max_evs);
+      memset(events, 0, sizeof(struct epoll_event) * m_max_evs);
+
+      while(m_run.load()){
+        errno = 0;
+        nfds = epoll_wait(m_poll_fd, events, m_max_evs, 5);
+	      if (nfds == -1 && errno == EBADF) {
+          std::cerr << "Error epoll_wait: " << errno << " " << strerror(errno) << "\n";
+			    break;
+		    }
+
+		    for (int n = 0; n < nfds; ++n) {
+          if (events[n].events & EPOLLIN) {
+            m_queue_fds->push_back(events[n].data.fd);
+            if(m_debug >= 4)
+              std::cout << "Epoll run, " << "EPOLLIN " << events[n].data.fd << "\n";
+          }
+        }
+      }
+
+      free(events);
+      m_run.store(false);
+      try { close(m_poll_fd); } catch (...) {} 
+    }
+
+    void shutdown(){
+      m_run.store(false);
+      try { close(m_poll_fd); } catch (...) {} 
+    }
+
+    virtual ~Epoll(){
+      shutdown();
+    }
+
+  private:
+
+    std::atomic<bool>      m_run=true;
+
+    int m_poll_fd, m_max_evs;
+    QueueFdPtr m_queue_fds;
+
+    int m_debug;
+};
+typedef std::shared_ptr<Epoll> EpollPtr;
+// Epoll end
+
+
+
+class UdpHandlerDest {
+
+  public:
+
+    UdpHandlerDest(int sock_fileno, 
+                   int send_reactors, int send_flags, 
+                   int ev_fileno, int recv_reactors, int recv_sockets_a_reactor,
+                   bool debug) : m_debug(debug) {
+      
+      // UdpDispatcher threads setup
+      if(send_reactors > 0){
+        UdpDispatcherPtr d_ptr;
+        for(int t=0;t<send_reactors;t++){ 
+          d_ptr = std::make_shared<UdpDispatcher>(sock_fileno, send_flags, m_queue_dispatch, debug);
+          std::thread ( [d=d_ptr]{ d->run(); } ).detach();
+          m_dispatchers.push_back(d_ptr);
+        }
+      }
+
+      // UdpReceiver threads setup
+      if(recv_reactors > 0){
+        if(ev_fileno == -1)
+          std::cerr << "Error ev_fileno is '-1' ";
+
+        // Get size for receiving buffer
+        int sendbuff = 512;
+        socklen_t optlen = sizeof(sendbuff);
+        if(getsockopt(sock_fileno, SOL_SOCKET, SO_RCVBUF, &sendbuff, &optlen) == -1)
+          std::cerr << "Error getsockopt: SO_RCVBUF ";
+        
+        UdpReceiverPtr r_ptr;
+        for(int t=0;t<recv_reactors;t++){
+          r_ptr = std::make_shared<UdpReceiver>(ev_fileno, sendbuff, m_queue_received, m_queue_fds, debug);
+          std::thread ( [d=r_ptr]{ d->run(); } ).detach();
+          m_receivers.push_back(r_ptr);
+        }
+
+        // Epoll threads setup
+        int epollfd = epoll_create(recv_reactors);
+        if (epollfd == -1) {
+          std::cerr << "Error epoll_create1";
+          recv_reactors=0;;
+        }
+
+        struct epoll_event ev;
+        for(int t=0; t < recv_reactors * recv_sockets_a_reactor; t++){
+
+          ev = {};
+          ev.events = EPOLLIN;
+	        ev.data.fd = dup(sock_fileno);   
+
+          make_socket_nonblocking(ev.data.fd);
+	        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, ev.data.fd, &ev) == -1) {
+            std::cerr << "Error epollf_ctl:" << epollfd << "\n";
+		        return;
+	        }
+        }
+
+        EpollPtr p_ptr;
+        p_ptr = std::make_shared<Epoll>(epollfd, recv_reactors, m_queue_fds, debug);
+        std::thread ( [d=p_ptr]{ d->run(); } ).detach();
+        m_epolls.push_back(p_ptr);
+      }
+
+    }
+
+   // DISPATCH
+    void dispatch(std::string addr, int port, std::string data){
+      QueueItemMsg msg = {
+        .addr = addr,
+        .port = port,
+        .data = data,
+        .ts_ns = ts_ns_now()
+      };
+      m_queue_dispatch->push_back(msg);
+      
+      if(m_debug >= 5)
+        std::cout << "UdpHandlerDest dispatch, " 
+                  << " addr " << msg.addr 
+                  << " port " << msg.port 
+                  << " data.length " << msg.data.length()
+                  << " ts_ns " << msg.ts_ns 
+                  << "\n";
+    }
+
+    size_t queued_send(){
+      return m_queue_dispatch->size();
+    }
+
+    u_int32_t stats_send_avg(bool reset){
+      u_int32_t sum = 0, count = 0, c;
+
+      for(auto &d : m_dispatchers){
+          c = d->m_stat_sendto_c.load();
+          count += c;
+          sum += d->m_stat_sendto_avg.load()*c;
+
+          d->m_stat_sendto_c.store(0);
+          d->m_stat_sendto_avg.store(0);
+      }
+      if(count==0)return 0;
+      return sum/count;
+    }
+    // DISPATCH
+
+
+    // RECEIVE
+    QueueItemMsg get_recved(){
+      return m_queue_received->pop_first();
+    }
+
+    size_t queued_recved(){
+      return m_queue_received->size();
+    }
+    // RECEIVE
+
+
+    operator UdpHandlerDest*() { 
+      return this;
+    }
+
+    void shutdown(){
+      if(m_debug >= 1)
+        std::cout << "UdpHandlerDest shutting down" << "\n";
+
+      for(auto &d : m_epolls)
+          d->shutdown();
+      m_queue_fds->stopping();
+
+      for(auto &d : m_dispatchers)
+          d->shutdown();
+      m_queue_received->stopping();
+
+      for(auto &d : m_receivers)
+          d->shutdown();
+      m_queue_dispatch->stopping();
+
+      if(m_debug >= 1)
+        std::cout << "UdpHandlerDest shutdown" << "\n";
+    }
+
+    virtual ~UdpHandlerDest(){
+      shutdown();
+    }
+
+  private:
+
+    std::vector<std::shared_ptr<UdpDispatcher>> m_dispatchers;
+    QueueMsgPtr m_queue_dispatch = std::make_shared<Queue<QueueItemMsg>>();
+
+    std::vector<std::shared_ptr<UdpReceiver>> m_receivers;
+    QueueMsgPtr m_queue_received = std::make_shared<Queue<QueueItemMsg>>();
+    
+    std::vector<std::shared_ptr<Epoll>> m_epolls;
+    QueueFdPtr m_queue_fds = std::make_shared<Queue<QueueItemFd>>();
+
+    int m_debug;
+};
+
+
+}  // namespace PyHelpers
+
+
+
+PYBIND11_MODULE(udp_handler_dest, m) {
+  m.doc() = "Python Helper for receiving and sending udp messages without blocking/waiting";
+
+  py::class_<PyHelpers::UdpHandlerDest, std::shared_ptr<PyHelpers::UdpHandlerDest>>(m, "UdpHandlerDest")
+    .def(py::init<int, int, int, int, int, int, int>(),        
+        py::arg("sock_fd"), 
+        py::arg("send_reactors") = 1, 
+        py::arg("send_flags") = 0,       
+        py::arg("event_fd") = -1, 
+        py::arg("recv_reactors") = 0,
+        py::arg("recv_sockets_a_reactor") = 4,
+        py::arg("debug_level") = 0
+        )
+
+    .def("dispatch", &PyHelpers::UdpHandlerDest::dispatch, py::arg("ip"), py::arg("port"), py::arg("data") )
+    
+    .def("get_recved", [](PyHelpers::UdpHandlerDest* hdlr) {
+      PyHelpers::QueueItemMsg msg = hdlr->get_recved();
+      if(msg.addr.empty()) 
+        return py::make_tuple();
+      return py::make_tuple(msg.addr, msg.port, msg.data, PyHelpers::ts_ns_now()-msg.ts_ns);
+    })
+
+    .def("queued_send", &PyHelpers::UdpHandlerDest::queued_send)
+    .def("stats_send_avg",  &PyHelpers::UdpHandlerDest::stats_send_avg,  py::arg("reset") = true)
+
+    .def("queued_recved",  &PyHelpers::UdpHandlerDest::queued_recved)
+    
+    .def("shutdown", &PyHelpers::UdpHandlerDest::shutdown)
+  ;
+}
---
+++ tests/test_udp_handler_dest.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+# AUTHOR Kashirin Alex (kashirin.alex@gmail.com) #
+
+from pyhelpers.udp_handler_dest import UdpHandlerDest
+import eventlet
+from eventlet import EventFd, greenthread
+import socket
+import psutil
+import time
+ev_sleep = eventlet.sleep
+
+host_ip = "127.0.0.1"
+host_port = 1100
+
+
+with_req_load = False
+num_threads = 8
+
+clt_threads = 4
+srv_greenthreads_process = 8
+recv_reactors  = 2
+recv_sockets_a_reactor = 4
+send_reactors = 2
+
+test_duration = 60  # secs
+
+p = psutil.Process()
+udp_socks = []
+ports = []
+dispatchers = []
+threads = []
+conf = {'c': 0, 'queued_recved.sum': 0, 'queued_recved.c': 0}
+integrity = {}
+to_run = True
+client_af = socket.AF_INET if '.' in host_ip else socket.AF_INET6
+
+send_bytes_clt = 32
+send_bytes_srv = 1024*send_bytes_clt
+recv_buf_clt = 32*1024
+send_buf_srv = 32*1024
+
+
+def get_stats_recv_avg():
+    if conf['queued_recved.c'] == 0:
+        return 0
+    return conf['queued_recved.sum']/conf['queued_recved.c']
+
+
+def proc_state():
+    print ('num_threads', p.num_threads(), 'cpu_percent', p.cpu_percent(),
+           'queued_send', handler.queued_send(), 'queued_recved', handler.queued_recved(),
+           'stats_send_avg', handler.stats_send_avg(True),
+           'stats_recv_avg', get_stats_recv_avg())
+    #
+
+#
+
+udp_s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+udp_s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, send_buf_srv)
+udp_s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 512)
+# udp_s.settimeout(5)
+
+port = host_port
+ports.append(port)
+udp_s.bind(('::', port))
+udp_s.setblocking(0)
+
+event_fd = EventFd(semaphore=False)
+handler = UdpHandlerDest(udp_s.fileno(),
+                         send_reactors=send_reactors, send_flags=0x40,
+                         event_fd=event_fd.dup(), recv_reactors=recv_reactors,
+                         recv_sockets_a_reactor=recv_sockets_a_reactor,
+                         debug_level=True)
+print (event_fd.fileno(), udp_s.fileno(), handler)
+udp_s.close()
+
+
+def process_req(req):
+    c_addr, c_port, data, ns_passed = req
+
+    if with_req_load:
+        '..'.join([str(a) for a in range(8096)])
+    integrity[data]['s'] = len(data)
+    handler.dispatch(c_addr, c_port, data.zfill(send_bytes_srv))
+    conf['queued_recved.c'] += 1
+    conf['queued_recved.sum'] += ns_passed
+
+
+def run_receive_events():
+    try:
+        print ('run_receive_events')
+        while to_run:
+            count = event_fd.receive()
+            # print ('event_fd.receive', count)
+            for v in range(count):
+                req = handler.get_recved()
+                if not req:
+                    print ('get_recved', req)
+                    continue
+                greenthread.spawn_n(process_req, req)
+    except Exception as e:
+        print (e)
+    #
+greenthread.spawn_n(run_receive_events)
+
+#
+
+
+def run_clt_sock(tn, addr, run):
+    print ('running py thread', 'clt', tn, addr)
+    udp_s = socket.socket(client_af, socket.SOCK_DGRAM)
+    udp_s.settimeout(25)
+    while run():
+        try:
+            conf['c'] += 1
+            uid = ("%d-%d|" % (conf['c'], tn)).zfill(send_bytes_clt).encode('utf-8')
+            integrity[uid] = {'c': 0, 's': 0}
+            udp_s.sendto(uid, addr)
+            data, server = udp_s.recvfrom(recv_buf_clt)
+            integrity[uid]['c'] = len(data)
+        except Exception as e:
+            print ('clt', tn, e)
+            break
+        time.sleep(0)
+    udp_s.close()
+    print ('stopped py thread', 'clt', tn, addr)
+    #
+
+import threading
+proc_state()
+clt_n = 0
+for srv_port in ports:
+    for n in range(1, clt_threads+1):
+        clt_n += 1
+        threading.Thread(target=run_clt_sock, name=str(clt_n),
+                         args=[clt_n, (host_ip, srv_port), lambda: to_run]).start()
+proc_state()
+
+
+#
+# WAIT
+print ('waiting')
+for w in range(num_threads):
+    ev_sleep(test_duration/num_threads)
+    proc_state()
+
+q = handler.queued_send() + handler.queued_recved()
+if q > 0:
+    proc_state()
+    ev_sleep(0.001*q)
+
+ev_sleep(3)
+to_run = False
+ev_sleep(3)
+
+for s in udp_socks:
+    try:
+        s.close()
+    except:
+        pass
+    ev_sleep(0)
+ev_sleep(3)
+#
+
+print ('*'*80)
+proc_state()
+ev_sleep(3)
+handler.shutdown()
+ev_sleep(10)
+proc_state()
+print ('*'*80)
+
+
+ev_sleep(10)
+f = True
+for n in integrity:
+    if send_bytes_srv != integrity[n]['c'] and send_bytes_clt == integrity[n]['s']:
+        f = False
+        print ("clt_n", n.split(b'|')[0], "recv", integrity[n])
+
+ev_sleep(3)
+f = f and integrity and p.num_threads() == 1
+
+
+print ('*'*80)
+print ("TEST: "+("PASS" if f else "FAIL")+"!", 'trx', len(integrity), 'num_threads', p.num_threads())
+print ('*'*80)
+
+if not f:
+    exit(1)
+
+#               original_udp_send | dispatcher
+# py-2 trx      863544              544415
+# py-3 trx      905657              363847
+# pypy trx      229255              171673
+
+#
